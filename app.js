f61e86ce7d06b4417f6bc591bc227fabfef0aefb
wardis <wardi2976@live.fr>
2020-11-05 01:12:50 +0000

    add smooth transition on animation

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const container = document.querySelector('.container');
 container.addEventListener('mousemove', (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-
   card.style.transform = `rotateY(${-xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+container.addEventListener('mouseenter', (e) => {
+  card.style.transition = 'none';
+});
+container.addEventListener('mouseleave', (e) => {
+  card.style.transition = 'all 0.5s ease';
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 });
